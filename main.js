a37d7092bc9196ae2b9956bb6362206602c9e94e
AliAbbara <abbara21@gmail.com>
2022-10-06 20:04:29 -0300

    .

---
+++ main.js
@@ -0,0 +1,7 @@
+var whatsappLink = document.getElementsByClassName("wp-icon");
+var instagramLink = document.getElementsByClassName("inst-icon");
+var facebookLink = document.getElementsByClassName("fb-icon");
+
+whatsappLink.onclick = function () {
+  window.location.href = "http://whatsapp.com";
+};
