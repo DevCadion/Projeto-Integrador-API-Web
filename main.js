f691eddda29a3f8505538a34c962001f5f0f2b37
AliAbbara <abbara21@gmail.com>
2022-11-20 13:43:19 -0300

    .

--- main.js
+++ main.js
@@ -17,3 +17,20 @@ window.onclick = function (event) {
     }
   }
 };
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {
+    slideIndex = 1;
+  }
+  x[slideIndex - 1].style.display = "block";
+  setTimeout(carousel, 1600); // Change image every /**/ seconds
+}
