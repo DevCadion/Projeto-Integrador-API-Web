b4c60f06e5776ab54945dfe3ed6e1ecec0c5d43b
AliAbbara <abbara21@gmail.com>
2022-11-22 15:34:38 -0300

    .

--- main.js
+++ main.js
@@ -17,16 +17,22 @@ window.onclick = function (event) {
   }
 };
 
-function search_animal() {
-  let input = document.getElementById("searchbar").value;
-  input = input.toLowerCase();
-  let x = document.getElementsByClassName("animals");
+function searchFunction() {
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("searchInput");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName("li");
 
-  for (i = 0; i < x.length; i++) {
-    if (!x[i].innerHTML.toLowerCase().includes(input)) {
-      x[i].style.display = "none";
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
     } else {
-      x[i].style.display = "list-item";
+      li[i].style.display = "none";
     }
   }
 }
